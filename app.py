c28d3234285054a690bf1ac745ad4cd7bbdc6176
James Woglom <j@wogloms.net>
2021-08-24 00:51:14 -0400

    destinations: parse MTA JS sourcemap containing station destinations

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 from flask import Flask, Response, request, abort, render_template, jsonify
-from underground import SubwayFeed
+from flask_cors import CORS
+from underground import SubwayFeed, metadata
+
 import csv
+import json
 
 app = Flask(__name__, static_url_path='/static')
+CORS(app)
 
 # Log messages with Gunicorn
 if not app.debug:
     import logging
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.INFO)
+
+destinations = json.loads(open("data/destinations.json").read())
+# from destinations import build_destinations_map
+# destinations = build_destinations_list()
 
 def build_stations_map():
     stations = {}
@@ -19,20 +27,86 @@ def build_stations_map():
         if stop["location_type"] == "1":
             continue
 
+        direction = stop["stop_id"][-1]
+        label = direction
+        routes = []
+
+        dest = destinations.get(stop["stop_id"][:-1])
+        if dest:
+            if direction == "N":
+                label = dest["North_Direction_Label"]
+            elif direction == "S":
+                label = dest["South_Direction_Label"]
+            
+            routes = dest["Daytime_Routes_Array"]
+
         stations[stop["stop_id"]] = {
             "name": stop["stop_name"],
-            "direction": stop["stop_id"][-1]
+            "direction": direction,
+            "destination": label,
+            "routes": routes
         }
     
     return stations
 
 stations = build_stations_map()
 
+def extract_stop_dict(self, timezone=metadata.DEFAULT_TIMEZONE):
+    from operator import attrgetter
+    import pydantic
+    import pytz
+
+    # filter down data to trips with an update
+    entities_with_updates = filter(lambda x: x.trip_update is not None, self.entity)
+    trip_updates = map(attrgetter("trip_update"), entities_with_updates)
+
+    # grab the updates with routes and stop times
+    trip_updates_with_stops = filter(
+        lambda x: x.trip.route_is_assigned and x.stop_time_update is not None,
+        trip_updates,
+    )
+    # create (route, stop, time) tuples from each trip
+    stops_flat = (
+        (
+            trip.trip.route_id_mapped,
+            stop.stop_id,
+            (stop.depart_or_arrive).time.astimezone(pytz.timezone(timezone)),
+            trip.trip
+        )
+        for trip in trip_updates_with_stops
+        for stop in trip.stop_time_update
+        if stop.depart_or_arrive is not None
+        and stop.depart_or_arrive.time >= self.header.timestamp
+    )
+
+    # group into a dict like {route: stop: [t1, t2]}
+    stops_grouped = dict()
+
+    for route_id, stop_id, departure, trip in stops_flat:
+        if route_id not in stops_grouped:
+            stops_grouped[route_id] = dict()
+
+        if stop_id not in stops_grouped[route_id]:
+            stops_grouped[route_id][stop_id] = []
+
+        stops_grouped[route_id][stop_id].append({
+            "time": departure,
+            "trip": {
+                "trip_id": trip.trip_id,
+                "start_time": str(trip.start_time),
+                "start_date": trip.start_date,
+                "route_id": trip.route_id
+
+            }
+        })
+
+    return stops_grouped
+
 def get_lines(lines):
     stop_dict = {}
     for l in lines:
         feed = SubwayFeed.get(l)
-        stop_dict.update(feed.extract_stop_dict())
+        stop_dict.update(extract_stop_dict(feed))
 
     stops = {}
     for l in stop_dict:
@@ -40,7 +114,7 @@ def get_lines(lines):
         for s in stop_dict[l]:
             stops[l][s] = {
                 "station": stations.get(s),
-                "stops": [{"time": str(dt), "line": l} for dt in stop_dict[l][s]]
+                "stops": [{"time": str(dt["time"]), "trip": dt["trip"]} for dt in stop_dict[l][s]]
             }
     
     return stops
@@ -70,12 +144,6 @@ def get_stations(lines, stations):
                     data[s] = stops[l][s]
                 else:
                     data[s]['stops'] = sort_stops(data[s]['stops'] + stops[l][s]['stops'])
-                
-                if len(stops[l][s]) > 0:
-                    if not 'lines' in data[s]:
-                        data[s]['lines'] = [l]
-                    else:
-                        data[s]['lines'].append(l)
     return data
 
 def sort_stops(stops):
---
+++ destinations.py
@@ -0,0 +1,72 @@
+import requests
+import urllib
+import sourcemap
+import io
+import pyjson5
+
+from bs4 import BeautifulSoup
+
+MTA_MAP = 'https://map.mta.info'
+
+def get_mtamap_js_url():
+    mtamap = requests.get(MTA_MAP)
+    soup = BeautifulSoup(mtamap.text, 'html.parser')
+    for s in soup.find_all('script'):
+        if 'src' in s.attrs and 'js/main' in s.attrs['src']:
+            return urllib.parse.urljoin(MTA_MAP, s.attrs['src'])
+
+def get_srcmap(jsurl, js):
+    mappath = sourcemap.discover(js)
+    path = urllib.parse.urljoin(jsurl, mappath)
+    mapcontents = requests.get(path).text
+    index = sourcemap.load(io.StringIO(mapcontents))
+    return index
+
+def find_js_define(index, jsc):
+    tokens = list(filter(None, [x if x.name == 'stationsUnified' and x.src.endswith('stations-unified.ts') else None for x in index.index.values()]))
+    if len(tokens) != 1:
+        return None
+    token = tokens[0]
+    jslines = jsc.splitlines()
+    ln = jslines[token.dst_line]
+    tk = ln[token.dst_col:]
+    return tk
+
+def find_dest_list(define):
+    values = define.split('=', 1)[1]
+    stk = []
+    OPEN = '[{'
+    CLOSE = ']}'
+    for i, v in enumerate(values):
+        if v in '[{':
+            stk.append(v)
+        elif v in ']}':
+            p = stk.pop()
+            if CLOSE.index(v) != OPEN.index(p):
+                raise ValueError('unexpected brace')
+        
+        if len(stk) == 0:
+            return values[:1+i]
+
+def parse_js(raw):
+    return pyjson5.decode(raw)
+
+def build_destinations_map():
+    jsurl = get_mtamap_js_url()
+    jscontents = requests.get(jsurl).text
+    index = get_srcmap(jsurl, jscontents)
+    define = find_js_define(index, jscontents)
+    dests_raw = find_dest_list(define)
+    dests_list = parse_js(dests_raw)
+    
+    dests = {}
+    for item in dests_list:
+        dests[item['stopId']] = item
+
+    return dests
+
+if __name__ == '__main__':
+    import pprint
+    pprint.pprint(build_destinations_map())
+    # import json
+    # open("data/destinations.json", "w").write(json.dumps(build_destinations_map()))
